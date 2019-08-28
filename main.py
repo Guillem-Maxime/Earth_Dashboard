665c136966ec6449fad4308592f8e963a28b75ac
Guillem-Maxime <max.guillem@hotmail.fr>
2019-08-28 19:37:41 +0200

    commented fullscreen command

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame
 pygame.init()
 
 gameDisplay = pygame.display.set_mode((800,600))
+#gameDisplay = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 pygame.display.set_caption('A bit Racey')
 
 clock = pygame.time.Clock()
