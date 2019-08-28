468acb6528dffc49ade9a82a64bf8b6b166dc746
Guillem-Maxime <max.guillem@hotmail.fr>
2019-08-28 18:40:37 +0200

    adding event loop to main

--- main.py
+++ main.py
@@ -4,3 +4,16 @@ pygame.init()
 
 gameDisplay = pygame.display.set_mode((800,600))
 pygame.display.set_caption('A bit Racey')
+
+clock = pygame.time.Clock()
+
+exited = False
+
+while not exited:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exited = True
+        print(event)
+
+    pygame.display.update()
+    clock.tick(60)
