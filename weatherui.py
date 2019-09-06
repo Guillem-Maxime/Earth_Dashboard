97d26b1648a7f0fd9497ae86e58ce8ecd670154e
Guillem-Maxime <max.guillem@hotmail.fr>
2019-09-06 18:55:50 +0200

    trying to update weather, but bad API call apparently

--- weatherui.py
+++ weatherui.py
@@ -1,10 +1,25 @@
 import pygame
+from pyowm import OWM
 
 from uisurfacebase import UISurfaceBase
+
+#Get an API key otherwise it might not work
+API_KEY = 'a8d618e73ff68e0fcfeae966ba48eba9'
+
+PLACE_01 = 'Montpellier,FR'
+PLACE_02 = 'Montreuil,FR'
+PLACE_03 = 'Manosque,FR'
+PLACE_04 = 'La Mure,FR'
 
 class WeatherUI(UISurfaceBase):
     def __init__(self, surface, topLeftPosition, gameDisplay):
         super().__init__(surface, topLeftPosition, gameDisplay)
+        self.InitOWM()
+
+    def InitOWM(self):
+        self.m_Owm = OWM(API_KEY)
+        self.m_Owm = OWM(language='fr')
+        self.m_ObsList = self.m_Owm.weather_at_place(PLACE_01)
 
     def GetColor(self):
         return (0,255,0)
